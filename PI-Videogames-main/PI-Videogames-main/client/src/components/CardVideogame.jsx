f5944804904b9de8090569cddae1650acd7be620
Irupe <lilianirupemeza@gmail.com>
2022-08-03 17:15:47 -0300

    carga la card y renderiza en home

--- PI-Videogames-main/PI-Videogames-main/client/src/components/CardVideogame.jsx
+++ PI-Videogames-main/PI-Videogames-main/client/src/components/CardVideogame.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { getVideogameDetail } from "../actions";
+import { getVideogameDetail, getVideogames } from "../actions";
 import "./CardVideogame.css";
 
 export default function CardVideogame(props) {
+  console.log(props);
   // quiero que me despache esta accion que me lleve al detalle del game
   const dispatch = useDispatch();
   function handleClick(e) {
     e.preventDefault();
-    dispatch(getVideogameDetail(props.id));
+    dispatch(getVideogames(props));
   }
 
   return (
-    <div className="containerCard">
-      <img
-        src={props.image}
+    <div key={props.id} className="containerCard">
+      {/* {
+        props.image 
+        ? <img
+          src={props.image}
+          alt="imagen de videojuego"
+          width="300px"
+          height="200px"
+        />
+        : <img
+        src="../letrero-neon-videojuegos_36298-743 (1).png"
         alt="imagen de videojuego"
         width="300px"
         height="200px"
       />
+      } */}
+      <img
+          src={props.image}
+          alt="imagen de videojuego"
+          width="300px"
+          height="200px"
+        />
       <h3 className="title">{props.name}</h3>
       {props.genres.map((g) => {
-        return <h5 key={g.name}>{g.name}</h5>;
+        return (<h5 key={g.name}>{g}</h5>);
       })}
+      <h6>{props.rating}</h6>
       <Link to={`/videogames/${props.id}`}>
         <button
           className="buttonCard"
